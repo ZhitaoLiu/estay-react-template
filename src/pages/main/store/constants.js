d267141dfe4a7317aefd45b7cf2226025ca5400f
刘志涛 <damon.liu@estay.com>
2020-12-16 18:13:34 +0800

    fix: redux constant rename

--- src/pages/main/store/constants.js
+++ src/pages/main/store/constants.js
@@ -1,3 +1,1 @@
-export const CHANGE_TOP_BANNER = 'recommend/CHANGE_TOP_BANNER';
-export const CHANGE_HOT_RECOMMEND = 'recommend/CHANGE_HOT_RECOMMEND';
-export const CHANGE_RANKING_LIST = 'recommend/CHANGE_RANKING_LIST';
+export const CHANGE_TOP_BANNER = 'Main/CHANGE_TOP_BANNER';
